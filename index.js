96b4b9719368652197b07398c5dad7304fded789
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-05-31 20:26:20 +0200

    chore(): update package

--- index.js
+++ index.js
@@ -4,43 +4,12 @@
 
 'use strict'
 
-const path = require('path')
 const config = require('cosmiconfig')
 
+const loadPlugins = require('./lib/loadPlugins')
+
 module.exports = function (options) {
-  function loadPlugins (config) {
-    if (typeof options === 'string') {
-      options = require(path.join(process.cwd(), options)).plugins
-    }
-
-    if (typeof options === 'object') {
-      for (let option in options) {
-        config.plugins[option] = options[option]
-      }
-    }
-
-    if (!options) {
-      options = config.plugins
-    }
-
-    function Plugin (plugin, options) {
-      if (options === null || options === undefined) {
-        return require(plugin)()
-      }
-      return require(plugin)(options)
-    }
-
-    let plugins = []
-
-    Object.keys(options).forEach((plugin) => {
-      plugins.push(Plugin(plugin, options[plugin]))
-    })
-
-    return plugins
-  }
-
   return config('postcss')
     .catch((error) => console.log(error))
-    .then((result) => loadPlugins(result.config))
-    .then((plugins) => console.log(plugins))
+    .then((result) => loadPlugins(result.config, options))
 }
