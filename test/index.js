b2be56ca89ee4bec2e54515d26d97248344bcaed
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-07-07 19:19:47 +0200

    test(): add node v0.12 testing :(

--- test/index.js
+++ test/index.js
@@ -4,36 +4,41 @@
 
 'use strict'
 
-const test = require('ava')
+var test = require('ava')
 
-const { join } = require('path')
-const { readFileSync, writeFileSync } = require('fs')
+var fs = require('fs')
+var path= require('path')
 
-const fixtures = (file) => readFileSync(join(__dirname, 'fixtures', file))
-const expected = (file) => readFileSync(join(__dirname, 'expects', file))
+function fixtures (file) {
+  return fs.readFileSync(path.join(__dirname, 'fixtures', file))
+}
 
-const postcss = require('postcss')
-const pluginsrc = require('..')
+function expected (file) {
+  return fs.readFileSync(path.join(__dirname, 'expects', file))
+}
 
-test('1 - Load with default plugins ', (t) => {
-  pluginsrc().then((plugins) => {
+var postcss = require('postcss')
+var pluginsrc = require('..')
+
+test('1 - Load with default plugins ', function (t) {
+  pluginsrc().then(function (plugins) {
     t.is(expected('plugins.default.js'), plugins)
   })
 })
 
-test('2 - Load with custom plugins ', (t) => {
-  pluginsrc('postcss.config.js').then((plugins) => {
+test('2 - Load with custom plugins ', function (t) {
+  pluginsrc('postcss.config.js').then(function (plugins) {
     t.is(expected('plugins.custom.js'), plugins)
   })
 })
 
-test('3 - Process CSS with default plugins', (t) => {
-  pluginsrc().then((plugins) => {
+test('3 - Process CSS with default plugins', function (t) {
+  pluginsrc().then(function (plugins) {
     postcss(plugins)
       .process(fixtures('index.css'))
-      .then((result) => {
+      .then(function (result) {
         console.log(result.css)
-        writeFileSync('expects/index.css', result.css)
+        fs.writeFileSync('expects/index.css', result.css)
         t.is(expected('index.css'), result.css)
       })
   })
@@ -43,33 +48,33 @@ test('4 - Process SSS with custom plugins', (t) => {
   pluginsrc('postcss.config.js').then((plugins) => {
     postcss(plugins)
       .process(fixtures('index.css'))
-      .then((result) => {
+      .then(function (result) {
         console.log(result.css)
-        writeFileSync('expects/custom.css', result.css)
+        fs.writeFileSync('expects/custom.css', result.css)
         t.is(expected('custom.css'), result.css)
       })
   })
 })
 
-test('5 - Process SSS with default plugins', (t) => {
-  pluginsrc().then((plugins) => {
+test('5 - Process SSS with default plugins', function (t) {
+  pluginsrc().then(function (plugins) {
     postcss(plugins)
       .process(fixtures('index.sss'), {parser: require('sugarss')})
-      .then((result) => {
+      .then(function (result) {
         console.log(result.css)
-        writeFileSync('expects/index.sss.css', result.css)
+        fs.writeFileSync('expects/index.sss.css', result.css)
         t.is(expected('index.sss.css'), result.css)
       })
   })
 })
 
-test('6 - Process SSS with custom plugins', (t) => {
-  pluginsrc('postcssrc.json').then((plugins) => {
+test('6 - Process SSS with custom plugins', function (t) {
+  pluginsrc('postcssrc.json').then(function (plugins) {
     postcss(plugins)
       .process(fixtures('index.sss'), {parser: require('sugarss')})
-      .then((result) => {
+      .then(function (result) {
         console.log(result.css)
-        writeFileSync('expects/custom.sss.css', result.css)
+        fs.writeFileSync('expects/custom.sss.css', result.css)
         t.is(expected('custom.sss.css'), result.css)
       })
   })
