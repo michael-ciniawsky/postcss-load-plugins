f637d60fc9a89b452bf8f40c643e631958366a8e
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-10-11 03:34:28 +0200

    feat(): function support, jsdoc, cleanups

--- index.js
+++ index.js
@@ -1,19 +1,59 @@
 // ------------------------------------
-// #POSTCSS - LOAD PLUGINS
+// #POSTCSS - LOAD PLUGINS - INDEX
 // ------------------------------------
 
 'use strict'
 
-var config = require('cosmiconfig')
+const config = require('cosmiconfig')
 
-var loadPlugins = require('./lib/loadPlugins')
+const loadPlugins = require('./lib/plugins')
 
-module.exports = function (options) {
-  return config('postcss')
-    .catch(function (error) {
-      console.log(error)
+/**
+ * @author Michael Ciniawsky (@michael-ciniawsky) <michael.ciniawsky@gmail.com>
+ * @description Autoload Plugins for PostCSS
+ *
+ * @module postcss-load-plugins
+ * @version 1.0.0
+ *
+ * @requires cosmiconfig
+ * @requires ./lib/plugins.js
+ *
+ * @method pluginsrc
+ *
+ * @param  {Object} ctx Context
+ * @param  {String} path Directory
+ * @param  {Object} options Options
+ *
+ * @return {Array} config PostCSS Plugins
+ */
+module.exports = function pluginsrc (ctx, path, options) {
+  const defaults = {
+    cwd: process.cwd(),
+    env: process.env.NODE_ENV
+  }
+
+  ctx = Object.assign(defaults, ctx) || defaults
+  path = path || process.cwd()
+  options = options || {}
+
+  return config('postcss', options)
+    .load(path)
+    .then((result) => {
+      result = result.config || {}
+
+      return result
     })
-    .then(function (result) {
-      return loadPlugins(result.config, options)
+    .then((plugins) => {
+      if (typeof plugins === 'function') {
+        plugins = plugins(ctx)
+      }
+      if (typeof result === 'object') {
+        plugins = Object.assign(plugins, ctx)
+      }
+
+      return loadPlugins(plugins)
+    })
+    .catch((err) => {
+      console.log(err)
     })
 }
